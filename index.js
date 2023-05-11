24059b0efb7a4630269d69db130a6414f0c17ee6
blitzkrieg <oguzhankuslar@gmail.com>
2023-05-11 19:08:45 +0300

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send({ message: 'hello client!' });
+});
 
 app.listen(5000, () => {
     console.log('server is running on port 5000.');
